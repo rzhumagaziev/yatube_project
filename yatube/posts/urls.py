5d1ca5db800d8be4d089d23fd40f522976fab2fe
Рашид Жумагазиев <r.zhumagaziev@yandex.ru>
2023-02-08 21:59:16 +0300

    new_app

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+  # Главная страница
+    path('', views.index),
+    # Страница со списком сортов мороженого
+    path('group/', views.group_list),
+    path('group/<slug>/', views.group_detail)
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_list(request):
+    return HttpResponse('Список групп')
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_detail(request, slug):
+    return HttpResponse(f'Группа {slug}')
