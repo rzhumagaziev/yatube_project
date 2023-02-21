8212367f60ffd26bb071f0b5ff852ed8d2a0bd98
Рашид Жумагазиев <r.zhumagaziev@yandex.ru>
2023-02-21 20:38:28 +0300

    upload css

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,7 @@ User = get_user_model()
 class Group(models.Model):
     title = models.TextField()
     slug = models.SlugField()
+    
     description = models.TextField()
     
     def __str__(self) -> str:
@@ -15,7 +16,11 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    group = models.ForeignKey(Group, on_delete = models.CASCADE, blank=True, null=True,)
+    group = models.ForeignKey(
+        Group,
+        on_delete = models.CASCADE,
+        blank=True,
+        null=True,)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
